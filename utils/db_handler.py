6fae3770c2c8f667cd2031d69d4d9601e66f8c5b
Ocuatrec <o.cuatrix@gmail.com>
2017-09-15 13:46:00 +0200

    Added random id generator

--- utils/db_handler.py
+++ utils/db_handler.py
@@ -2,3 +2,16 @@ from sqlalchemy import or_
 from main import app,db
 from .models import *
 from .values import *
+from random import randint
+
+def createID(length = 6):
+	ret = ""
+	i = 0
+	for i in range(length):
+		n = randint(0, 35)
+		if n > 25:
+			n += 22
+		else:
+			n += 97
+		ret = ret[:i] + str(chr(n)) + ret[i + 1:]
+	return ret

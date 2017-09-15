0b91764d55d1345c1995e5a124987ab216fbe0f9
PolBaladas <polbaladasluna@gmail.com>
2017-09-15 09:34:25 +0200

    index.html

--- utils/models.py
+++ utils/models.py
@@ -1,1 +1,15 @@
 from main import app, db
+
+class Story(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	full_text = db.Column(db.Text)
+
+	def __init__(self, full_text):
+		self.full_text = full_text
+
+class Tweet(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(140))
+
+	def __init__(self, text):
+		self.text = text

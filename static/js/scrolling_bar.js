5e0e6f8725ec57fbb2fcc80c22003878e3559bc4
Ocuatrec <o.cuatrix@gmail.com>
2017-09-15 19:02:28 +0200

    Added both methods for the progress bar. Will choose later

--- static/js/scrolling_bar.js
+++ static/js/scrolling_bar.js
@@ -6,8 +6,24 @@ function tweetProgressBar() {
   var len = textarea.value.length;
   width = Math.round(len / 1.4);
   bar.style.width = width + '%';
+  
+  /* GRADIENT HUE */
+  /*
   hue = 78 - 0.0078 * Math.pow(width, 2);
   bar.style.backgroundColor = "hsl(" + hue + ",38.4%,47.2%)";
+  */
+
+  /* NO HUE */
+  /*
+  if (width == 100)
+  {
+    bar.style.backgroundColor = "#AF4B4B";
+  }
+  else
+  {
+    bar.style.backgroundColor = "#4CAF50";
+  }
+  */
 }
 
 document.getElementById("tweet-box").oninput = function () {tweetProgressBar()};

f7bee59e00b3a3914a3796270e3b4bbf5eb3613d
PolBaladas <polbaladasluna@gmail.com>
2017-09-15 13:02:01 +0200

    db migration

--- utils/models.py
+++ utils/models.py
@@ -5,8 +5,9 @@ class Story(db.Model):
 	title = db.Column(db.String(140))
 	full_text = db.Column(db.Text)
 
-	def __init__(self, full_text):
+	def __init__(self, full_text, title):
 		self.full_text = full_text
+		self.title = title
 
 class Tweet(db.Model):
 	id = db.Column(db.Integer, primary_key=True)

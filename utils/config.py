cb26ed0aeebcd0c105a5fa8eb8b802d2f9d9241e
Ocuatrec <o.cuatrix@gmail.com>
2017-12-05 11:49:01 +0100

    Uploaded config file

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+# dev
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+SECRET_KEY = 'dummy'
+SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DATABASE_CONNECT_OPTIONS = {}
